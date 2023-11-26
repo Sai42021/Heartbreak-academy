c9dc7af0867a98e702c0bdc0f4fd264c42b3bd27
Abisai Sam <ASam@schoemans.com.na>
2023-11-26 12:16:29 +0200

    Created on-scroll effect for tales

--- JS/main.js
+++ JS/main.js
@@ -1,26 +1,28 @@
-// Page Loader
- // Get a reference to the loader
-            var loader = document.querySelector('.page-loader');
-
-            // Show the loader when the page starts loading
-            window.addEventListener('loadstart', function () {
-                loader.style.display = 'block';
-            });
-
-            // Hide the loader when the page has finished loading
-            window.addEventListener('load', function () {
-                loader.style.display = 'none';
-            });
-
 // Fade-In-Up effect
-$(window).scroll(function() {
- $('.card-link').each(function() {
-  var bottom_of_object = $(this).offset().top + $(this).outerHeight();
-  var bottom_of_window = $(window).scrollTop() + $(window).height();
-  if (bottom_of_window > bottom_of_object) {
-    $(this).addClass('animate');
-    $(this).removeClass('card-link');
-  }
- });
-});
+// Function to check if an element is in the viewport
+        function isInViewport(element) {
+            const rect = element.getBoundingClientRect();
+            return (
+                rect.top >= 0 &&
+                rect.left >= 0 &&
+                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+                rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+            );
+        }
 
+        // Function to handle scroll events
+        function handleScroll() {
+            const cardLinks = document.querySelectorAll('.card-link');
+
+            cardLinks.forEach(cardLink => {
+                if (isInViewport(cardLink)) {
+                    cardLink.classList.add('fade-in-up');
+                }
+            });
+        }
+
+        // Attach the scroll event listener
+        document.addEventListener('scroll', handleScroll);
+
+        // Initial check on page load
+        handleScroll();
